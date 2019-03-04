8377f7fac35d1fe133bd1981c4628fa2b0cb19e8
senthil121 <senthilrock121@gmail.com>
2019-03-04 12:11:00 +0000

    this is ctosp

--- android/config.go
+++ android/config.go
@@ -28,7 +28,7 @@ import (
 	"github.com/google/blueprint/bootstrap"
 	"github.com/google/blueprint/proptools"
 
-	"aosp/soong/android"
+	"ctosp/soong/android"
 )
 
 var Bool = proptools.Bool
@@ -471,7 +471,7 @@ func (c *config) ResourceOverlays() []string {
 }
 
 func (c *config) JavaSourceOverlays() string {
-	return String(c.productVariables.Aosp.Java_Source_Overlays)
+	return String(c.productVariables.ctosp.Java_Source_Overlays)
 }
 
 func (c *config) JavaSourceOverlayModuleWhitelist() []string {
@@ -787,7 +787,7 @@ func (c *deviceConfig) DeviceKernelHeaderDirs() []string {
 }
 
 func (c *deviceConfig) SpecificCameraParametersLibrary() string {
-	return String(c.config.productVariables.Aosp.Specific_camera_parameter_library)
+	return String(c.config.productVariables.ctosp.Specific_camera_parameter_library)
 }
 
 func (c *deviceConfig) NativeCoverageEnabled() bool {
--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"aosp/soong/android"
+	"ctosp/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -118,7 +118,7 @@ type variableProperties struct {
 		}
 
 		// include custom variables
-		Aosp android.Product_variables
+		ctosp android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -237,7 +237,7 @@ type productVariables struct {
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
 	// include Custom variables
-	Aosp android.ProductVariables
+	ctosp android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
